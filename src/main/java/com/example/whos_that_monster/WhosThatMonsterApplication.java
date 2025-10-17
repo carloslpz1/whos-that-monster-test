86938b2cb4e0a44a1de4435161772020c705700b
carloslopez98 <carlos980718@gmail.com>
2025-10-16 19:36:24 -0500

    Initial commit

---
+++ src/main/java/com/example/whos_that_monster/WhosThatMonsterApplication.java
@@ -0,0 +1,13 @@
+package com.example.whos_that_monster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosThatMonsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhosThatMonsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whos_that_monster/WhosThatMonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whos_that_monster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhosThatMonsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
